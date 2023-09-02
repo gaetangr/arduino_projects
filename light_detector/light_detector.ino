680f98a65dc942944fe02ae65bf92d75aa038323
Gaëtan <gaetan.grond@gmail.com>
2023-09-02 21:04:33 +0200

    Update light_detector.ino

--- light_detector/light_detector.ino
+++ light_detector/light_detector.ino
@@ -1,6 +1,8 @@
-#include "DHT.h"
-#include "SR04.h"
+#include <NewPing.h>
 
+#include "DHT.h"
+
+#include <EEPROM.h>
 
 #define TRIG_PIN 13
 #define ECHO_PIN 12
@@ -9,15 +11,18 @@
 // Instances initialization
 //------------------------
 DHT dht(11, DHT11);
-SR04 sr04 = SR04(ECHO_PIN, TRIG_PIN);
+NewPing sonar(TRIG_PIN, ECHO_PIN, 500);
 
 // CONSTANTES
 // ------------------------
 long distance;
-bool DEBUG = false;
+unsigned long arduinoStartedTime;
+
+bool DEBUG = true;
 const int sensorPin = A0;
 const int sensorMiddlePin = A5;
-
+bool ligthOn = false;
+const int eepromAddr = 1;
 const int leftLed = 5;
 const int middleLed = 3;
 const int rightLed = 2;
@@ -25,8 +30,8 @@ const int rightLed = 2;
 const int alertRedLed = A4;
 const int alertBlueLed = A3;
 
-const int tempLimit = 27;
-const int distanceLimit = 50;
+const int tempLimit = 200;
+const int distanceLimit = 90;
 const long interval = 20000;
 const int delayBetweenLight = 500;
 int passage = 0;
@@ -39,10 +44,12 @@ int sensorMiddlevalue = 0;
 
 bool lightOn = false;
 
+unsigned long previousTime = 0;           // variable to store the previous time
+const unsigned long intervalFunc = 3400;  // interval of 30 minutes in milliseconds
 
 
 void switchOffLights() {
- 
+
   delay(delayBetweenLight);
   digitalWrite(leftLed, LOW);
   delay(delayBetweenLight);
@@ -62,41 +69,55 @@ void switchOnLights() {
 
 
 void setup() {
-  Serial.begin(9600);
 
+  Serial.begin(9600);
   // Initialize pins
   //-------------------
   pinMode(sensorPin, OUTPUT);
   pinMode(leftLed, OUTPUT);
   pinMode(middleLed, OUTPUT);
   pinMode(rightLed, OUTPUT);
-
-
   pinMode(alertBlueLed, OUTPUT);
   pinMode(alertRedLed, OUTPUT);
-
   dht.begin();
 }
 
+
+
 void loop() {
-  distance = sr04.Distance();
+  arduinoStartedTime = millis();
+  long minutesEllapses = arduinoStartedTime / 60000;
+  unsigned int distance = sonar.ping_cm();
   sensorValue = analogRead(sensorPin);
   sensorMiddlevalue = analogRead(sensorMiddlePin);
   int val = sensorValue + sensorMiddlevalue;
 
 
-  if (DEBUG) {
-    Serial.print("Distance cm: ");
-    Serial.println(distance);
-    Serial.print("Luminosity: ");
-    Serial.println(val);
-    Serial.print("Temperature: ");
-    Serial.println(t);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+
+  unsigned long currentTime = millis();              // get the current time
+  if (currentTime - previousTime >= intervalFunc) {  // check if the interval has passed
+    previousTime = currentTime;                      // update the previous time
+    EEPROM.write(eepromAddr, passage);
   }
 
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  Serial.println(sonar.ping_cm());
+  if (DEBUG) {
+    //Serial.print("Distance cm: ");
+    //Serial.println(distance);
+    // Serial.print("Luminosity: ");
+    //Serial.println(val);
+    // Serial.print("Temperature: ");
+    // Serial.println(t);
+    // Serial.print("Nombre de passages: ");
+    // Serial.println(passage);
+    //Serial.println(lightOn);
+  }
+
+
 
   if (t >= tempLimit) {
     digitalWrite(alertBlueLed, HIGH);
@@ -112,19 +133,18 @@ void loop() {
 
 
 
-  if (distance <= distanceLimit) {
-    passage += 1;
+  if (sonar.ping_cm() <= distanceLimit) {
+    passage++;
+    lightOn = !lightOn;
+    digitalWrite(alertRedLed, HIGH);
   }
 
-  if (passage > 1) {
+  if (!lightOn) {
     switchOffLights();
-    passage = 0;
-    delay(8000); // wait 20s to make sure no false positive
   }
 
-  if (distance <= distanceLimit && val <= 2) {
-    if (passage == 0) {
-     switchOnLights(); // keep light on
-    }
+  if (lightOn) {
+
+    switchOnLights();  // keep light on
   }
 }
